75c40ec20fef6fccc3f6a1bad30db558defd9cc7
Max Kellermann <mk@cm4all.com>
2022-03-20 07:24:32 +0100

    ext/opcache: build as static module
    This eliminates some overhead at PHP startup and solves linker
    problems when libstdc++ is linked statically.

--- ext/opcache/ZendAccelerator.c
+++ ext/opcache/ZendAccelerator.c
@@ -5039,7 +5039,7 @@ static void accel_activate(void) {
 	}
 }
 
-ZEND_EXT_API zend_extension zend_extension_entry = {
+ZEND_EXT_API zend_extension opcache_zend_extension_entry = {
 	ACCELERATOR_PRODUCT_NAME,               /* name */
 	PHP_VERSION,							/* version */
 	"Zend Technologies",					/* author */
