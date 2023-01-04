501257904434f2253f985f6485f615525149030d
Max Kellermann <max.kellermann@ionos.com>
2023-01-04 10:28:20 +0100

    Zend/zend_{operators,variables}: include cleanup

--- Zend/zend.c
+++ Zend/zend.c
@@ -36,6 +36,7 @@
 #include "zend_observer.h"
 #include "zend_fibers.h"
 #include "zend_max_execution_timer.h"
+#include "zend_strtod.h"
 #include "Optimizer/zend_optimizer.h"
 
 static size_t global_map_ptr_last = 0;
