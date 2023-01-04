87b8ca50b0110b3d10bd13fd7c5979a6cc5727b7
Max Kellermann <max.kellermann@ionos.com>
2023-01-04 21:45:32 +0100

    Zend/Optimizer/zend_ssa: include cleanup

--- Zend/Optimizer/zend_ssa.c
+++ Zend/Optimizer/zend_ssa.c
@@ -17,13 +17,14 @@
    +----------------------------------------------------------------------+
 */
 
-#include "zend_compile.h"
-#include "zend_dfg.h"
 #include "zend_ssa.h"
+#include "zend_arena.h"
+#include "zend_optimizer_internal.h"
+#include "zend_dfg.h"
 #include "zend_dump.h"
-#include "zend_inference.h"
+#include "zend_inference.h" // for zend_sub_will_overflow()
+#include "zend_type_info.h" // for MAY_BE_REF
 #include "zend_worklist.h"
-#include "Optimizer/zend_optimizer_internal.h"
 
 static bool dominates(const zend_basic_block *blocks, int a, int b) {
 	while (blocks[b].level > blocks[a].level) {
