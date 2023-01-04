32a1688908f48b85ca974e10720802b68f10ced1
Max Kellermann <max.kellermann@ionos.com>
2023-01-04 09:15:15 +0100

    Zend/zend_object_handlers: include cleanup

--- Zend/zend_object_handlers.c
+++ Zend/zend_object_handlers.c
@@ -18,14 +18,15 @@
    +----------------------------------------------------------------------+
 */
 
+#include "zend_object_handlers.h"
 #include "zend.h"
+#include "zend_error.h"
 #include "zend_globals.h"
 #include "zend_lazy_objects.h"
 #include "zend_variables.h"
 #include "zend_API.h"
 #include "zend_objects.h"
 #include "zend_objects_API.h"
-#include "zend_object_handlers.h"
 #include "zend_interfaces.h"
 #include "zend_exceptions.h"
 #include "zend_closures.h"
