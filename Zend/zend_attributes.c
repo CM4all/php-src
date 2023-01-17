3ace6c2ba4174142b8e2125f2483157f718986f3
Max Kellermann <max.kellermann@ionos.com>
2023-01-17 17:01:30 +0100

    Zend/zend_attributes: include cleanup

--- Zend/zend_attributes.c
+++ Zend/zend_attributes.c
@@ -17,11 +17,10 @@
    +----------------------------------------------------------------------+
 */
 
-#include "zend.h"
-#include "zend_API.h"
 #include "zend_attributes.h"
+#include "zend_API.h" // needed by zend_attributes_arginfo.h
 #include "zend_attributes_arginfo.h"
-#include "zend_exceptions.h"
+#include "zend_objects.h" // for zend_objects_new()
 #include "zend_smart_str.h"
 
 ZEND_API zend_class_entry *zend_ce_attribute;
