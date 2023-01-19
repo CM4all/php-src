337e1e94052d9e7af3271c4e88e3a9b29f439f91
Max Kellermann <max.kellermann@ionos.com>
2023-01-19 18:30:09 +0100

    Zend/zend_builtin_functions: include cleanup

--- Zend/zend_builtin_functions.c
+++ Zend/zend_builtin_functions.c
@@ -17,21 +17,22 @@
    +----------------------------------------------------------------------+
 */
 
-#include "zend.h"
-#include "zend_API.h"
-#include "zend_attributes.h"
-#include "zend_error.h"
-#include "zend_gc.h"
 #include "zend_builtin_functions.h"
+#include "zend.h" // for zend_standard_class_def
+#include "zend_API.h" // for ZEND_MINIT_FUNCTION
+#include "zend_attributes.h" // for zend_add_class_attribute()
+#include "zend_closures.h" // for zend_ce_closure
 #include "zend_constants.h"
-#include "zend_ini.h"
-#include "zend_interfaces.h"
-#include "zend_exceptions.h"
+#include "zend_error.h" // for zend_error()
+#include "zend_exceptions.h" // for zend_register_default_classes()
 #include "zend_extensions.h"
-#include "zend_closures.h"
 #include "zend_generators.h"
+#include "zend_globals.h" // for struct _zend_executor_globals
+#include "zend_globals_macros.h" // for EG()
+#include "zend_ini.h"
+#include "zend_interfaces.h" // for zend_call_method_with_1_params()
 #include "zend_builtin_functions_arginfo.h"
-#include "zend_smart_str.h"
+#include "zend_resource.h" // for struct _zend_resource, dereferenced by Z_RES_HANDLE_P()
 
 /* }}} */
 
