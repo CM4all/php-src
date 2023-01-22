bda796f280c247d9ec2f3d533d3a484e85615048
Max Kellermann <max.kellermann@ionos.com>
2023-01-22 08:30:39 +0100

    main/php.h: add #include directives that some extensions rely on
    These are currently indirectly included, but in order to retain
    source compatibility with out-of-tree extensions when #includes are
    cleaned up in those parts of PHP, we need to re-add them to the
    "php.h" header.

--- main/php.h
+++ main/php.h
@@ -441,4 +441,20 @@ typedef bool zend_bool;
 typedef intptr_t zend_intptr_t;
 typedef uintptr_t zend_uintptr_t;
 
+/* the following headers used to be included indirectly, and we have
+ * them here only for backwards compatibility with thirdparty
+ * extensions */
+#include "php_globals.h"
+#include "php_ini.h"
+#include "zend_alloc.h"
+#include "zend_arena.h"
+#include "zend_iterators.h"
+#include "zend_multiply.h"
+#include "zend_objects.h"
+#include "zend_strtod.h"
+#include "zend_types.h"
+#include <errno.h>
+#include <float.h>
+#include <string.h>
+
 #endif
