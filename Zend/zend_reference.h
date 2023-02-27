0f4fa2815bbdba8687dfc19e4c09ed6be52d568a
Max Kellermann <max.kellermann@ionos.com>
2023-02-27 20:56:47 +0100

    Zend/zend_types.h: move `zend_reference` to `zend_reference.h`

---
+++ Zend/zend_reference.h
@@ -0,0 +1,30 @@
+/*
+   +----------------------------------------------------------------------+
+   | Zend Engine                                                          |
+   +----------------------------------------------------------------------+
+   | Copyright (c) Zend Technologies Ltd. (http://www.zend.com)           |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.00 of the Zend license,     |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.zend.com/license/2_00.txt.                                |
+   | If you did not receive a copy of the Zend license and are unable to  |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@zend.com so we can mail you a copy immediately.              |
+   +----------------------------------------------------------------------+
+*/
+
+#ifndef ZEND_REFERENCE_H
+#define ZEND_REFERENCE_H
+
+#include "zend_property_info.h"
+#include "zend_refcounted.h"
+#include "zend_types.h"
+
+struct _zend_reference {
+	zend_refcounted_h              gc;
+	zval                           val;
+	zend_property_info_source_list sources;
+};
+
+#endif /* ZEND_REFERENCE_H */
