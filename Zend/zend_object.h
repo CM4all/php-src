1b6f8af45af061086872b4612a1fec8316e1487f
Max Kellermann <max.kellermann@ionos.com>
2023-02-27 20:12:52 +0100

    Zend/zend_types.h: move `struct _zend_object` to `zend_object.h`

---
+++ Zend/zend_object.h
@@ -0,0 +1,38 @@
+/*
+   +----------------------------------------------------------------------+
+   | Zend Engine                                                          |
+   +----------------------------------------------------------------------+
+   | Copyright (c) Zend Technologies Ltd. (http://www.zend.com)           |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.00 of the Zend license,     |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.zend.com/license/2_00.txt.                                |
+   | If you did not receive a copy of the Zend license and are unable to  |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@zend.com so we can mail you a copy immediately.              |
+   +----------------------------------------------------------------------+
+*/
+
+#ifndef ZEND_OBJECT_H
+#define ZEND_OBJECT_H
+
+#include "zend_refcounted.h"
+#include "zend_types.h"
+
+#include <stdint.h>
+
+typedef struct _zend_class_entry zend_class_entry;
+typedef struct _zend_object_handlers zend_object_handlers;
+typedef struct _zend_array HashTable;
+
+typedef struct _zend_object {
+	zend_refcounted_h gc;
+	uint32_t          handle; // TODO: may be removed ???
+	zend_class_entry *ce;
+	const zend_object_handlers *handlers;
+	HashTable        *properties;
+	zval              properties_table[1];
+} zend_object;
+
+#endif /* ZEND_OBJECT_H */
