0b62bfdd54575a33a311d42ad602325aac435889
Max Kellermann <max.kellermann@ionos.com>
2023-02-27 20:48:46 +0100

    Zend/zend_types.h: move `zend_type` to `zend_type.h`

--- Zend/zend_API.h
+++ Zend/zend_API.h
@@ -33,6 +33,7 @@
 #include "zend_portability.h" // for BEGIN_EXTERN_C
 #include "zend_operators.h"
 #include "zend_frameless_function.h"
+#include "zend_type.h"
 
 typedef struct _zend_module_entry zend_module_entry;
 
