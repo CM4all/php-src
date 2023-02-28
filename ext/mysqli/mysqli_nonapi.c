007b5423e50992698551b61a759db46f2af0a996
Max Kellermann <max.kellermann@ionos.com>
2023-02-28 05:56:36 +0100

    ext/mysqlnd: collect connect/query metrics (for WAS)

--- ext/mysqli/mysqli_nonapi.c
+++ ext/mysqli/mysqli_nonapi.c
@@ -28,6 +28,9 @@
 #include "zend_smart_str.h"
 #include "php_mysqli_structs.h"
 #include "mysqli_priv.h"
+#include "metrics.h"
+#include "wallclock.h"
+
 #define ERROR_ARG_POS(arg_num) (getThis() ? (arg_num-1) : (arg_num))
 
 #define SAFE_STR(a) ((a)?a:"")
@@ -64,7 +67,6 @@ void mysqli_common_connect(INTERNAL_FUNCTION_PARAMETERS, bool is_real_connect, b
 	zend_resource		*le;
 	mysqli_plist_entry *plist = NULL;
 	bool			self_alloced = 0;
-
 
 #if !defined(MYSQL_USE_MYSQLND)
 	if ((MYSQL_VERSION_ID / 100) != (mysql_get_client_version() / 100)) {
@@ -253,6 +255,11 @@ void mysqli_common_connect(INTERNAL_FUNCTION_PARAMETERS, bool is_real_connect, b
 		new_connection = true;
 	}
 
+	struct wallclock wc;
+	if (want_was_metrics) {
+		wallclock_start(&wc);
+	}
+
 #ifndef MYSQLI_USE_MYSQLND
 	/* BC for prior to bug fix #53425 */
 	flags |= CLIENT_MULTI_RESULTS;
@@ -311,6 +318,8 @@ void mysqli_common_connect(INTERNAL_FUNCTION_PARAMETERS, bool is_real_connect, b
 						port, socket, flags, MYSQLND_CLIENT_NO_FLAG) == NULL)
 #endif
 	{
+		++was_metrics.mysql_connect_errors;
+
 		/* Save error messages - for mysqli_connect_error() & mysqli_connect_errno() */
 		php_mysqli_set_error(mysql_errno(mysql->mysql), (char *) mysql_error(mysql->mysql));
 		php_mysqli_throw_sql_exception((char *)mysql_sqlstate(mysql->mysql), mysql_errno(mysql->mysql),
@@ -321,6 +330,11 @@ void mysqli_common_connect(INTERNAL_FUNCTION_PARAMETERS, bool is_real_connect, b
 			mysql->mysql = NULL;
 		}
 		goto err;
+	}
+
+	if (want_was_metrics) {
+		++was_metrics.mysql_connect_count;
+		was_metrics.mysql_connect_wait += wallclock_end(&wc);
 	}
 
 	/* clear error */
@@ -706,7 +720,6 @@ PHP_FUNCTION(mysqli_query)
 
 	MYSQLI_DISABLE_MQ;
 
-
 #ifdef MYSQLI_USE_MYSQLND
 	if (resultmode & MYSQLI_ASYNC) {
 		if (mysqli_async_query(mysql->mysql, query, query_len)) {
@@ -718,9 +731,20 @@ PHP_FUNCTION(mysqli_query)
 	}
 #endif
 
+	struct wallclock wc;
+	if (want_was_metrics) {
+		wallclock_start(&wc);
+	}
+
 	if (mysql_real_query(mysql->mysql, query, query_len)) {
+		++was_metrics.mysql_query_errors;
 		MYSQLI_REPORT_MYSQL_ERROR(mysql->mysql);
 		RETURN_FALSE;
+	}
+
+	if (want_was_metrics) {
+		++was_metrics.mysql_query_count;
+		was_metrics.mysql_query_wait += wallclock_end(&wc);
 	}
 
 	if (!mysql_field_count(mysql->mysql)) {
