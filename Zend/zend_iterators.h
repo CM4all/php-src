e48061814d36080ac45d98ac79082f1694107480
Max Kellermann <max.kellermann@ionos.com>
2023-03-07 15:15:59 +0100

    Zend/zend_types.h: move zval to zend_value.h

--- Zend/zend_iterators.h
+++ Zend/zend_iterators.h
@@ -21,8 +21,8 @@
 #define ZEND_ITERATORS_H
 
 #include "zend_long.h"
-#include "zend_types.h" // for zval, zend_object
 #include "zend_object.h"
+#include "zend_value.h"
 
 /* These iterators were designed to operate within the foreach()
  * structures provided by the engine, but could be extended for use
