1c8a06d66fb662252c287d383bfb30b86cb5677a
Max Kellermann <max.kellermann@ionos.com>
2024-10-15 16:05:20 +0200

    Zend/zend_gc: add struct tag to zend_get_gc_buffer
    Allow it to be forward-declared.

--- Zend/zend_gc.h
+++ Zend/zend_gc.h
@@ -107,7 +107,7 @@ static zend_always_inline void gc_check_possible_root_no_ref(zend_refcounted *re
  * over heterogeneous structures. See zend_generator_get_gc() for
  * a usage example. */
 
-typedef struct {
+typedef struct _zend_get_gc_buffer {
 	zval *cur;
 	zval *end;
 	zval *start;
