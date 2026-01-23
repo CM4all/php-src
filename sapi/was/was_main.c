e95e6733ebe5e688098a411691a57de4c1f7c106
Max Kellermann <max.kellermann@ionos.com>
2026-01-23 18:32:38 +0100

    sapi/was: remove redundant memset() before calling zend_stream_init_filename()
    zend_stream_init_filename() has the same memset() call.

--- sapi/was/was_main.c
+++ sapi/was/was_main.c
@@ -454,7 +454,6 @@ static zend_result was_module_main(struct was_simple *w)
 		return FAILURE;
 
 	zend_file_handle file_handle;
-	memset(&file_handle, 0, sizeof(file_handle));
 	zend_stream_init_filename(&file_handle, SG(request_info).path_translated);
 	php_execute_script(&file_handle);
 	zend_destroy_file_handle(&file_handle);
