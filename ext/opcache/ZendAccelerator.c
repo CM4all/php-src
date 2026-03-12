cfa7a39304a0cea95935ddae8ef856fc04f26d52
Max Kellermann <max.kellermann@ionos.com>
2026-03-12 13:28:41 +0100

    ext/opcache/ZendAccelerator: set status 404 if opening the primary script fails
    Another attempt to fix the HTTP status for non-existent scripts.  This
    time right after the error has occurred (and not later from the SAPI).

--- ext/opcache/ZendAccelerator.c
+++ ext/opcache/ZendAccelerator.c
@@ -2108,7 +2108,9 @@ static zend_op_array *persistent_compile_file_inner(zend_file_handle *file_handl
 		    if (file_handle->type == ZEND_HANDLE_FILENAME
 		     && accelerator_orig_zend_stream_open_function(file_handle) == FAILURE) {
 				if (!EG(exception)) {
-					if (type == ZEND_REQUIRE) {
+					if (file_handle->primary_script) {
+						SG(sapi_headers).http_response_code = 404;
+					} else if (type == ZEND_REQUIRE) {
 						zend_message_dispatcher(ZMSG_FAILED_REQUIRE_FOPEN, ZSTR_VAL(filename));
 					} else {
 						zend_message_dispatcher(ZMSG_FAILED_INCLUDE_FOPEN, ZSTR_VAL(filename));
